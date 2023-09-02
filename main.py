b7b0990e06e7f5c630383a5c0539998015cabf3f
Sibdroid <ryabovglebrg@gmail.com>
2023-09-02 17:29:13 +0200

    Added basic functionality for displaying colors.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import typing as t
-from datetime import datetime
+import calendar
+from PIL import Image, ImageDraw
 
 
 class Month:
 
     def __init__(self,
                  month: t.Union[int, str],
-                 year: int) -> None:
+                 year: int,
+                 colors: list[str],
+                 neutral_color: str="#EEEEEE") -> None:
         """Initializes an object of type 'Month'.
 
         Args:
@@ -24,13 +27,17 @@ class Month:
             year (int). An int in [0:99] or greater than or equal to 1000.
                 Converted to2000+year in former case,
                 parsed as is in latter case.
+            colors (str). A list of colors, each represented as a hex value.
+            neutral_color (str). The color used for days outside the month.
         Raises:
-            ValueError: in three cases.
+            ValueError: in four cases.
                 1) If month is given as a str, but it is not one of the
                    supported month names.
                 2) If month is given as an int, but it is outside [0; 11] range.
                 3) If year is outside the [0: 99] range and is not greater than
                    or equal to 1000.
+                4) If the amount of colors is not equal to the amount of
+                   days in the month.
             TypeError:
                 1) If month is not given as an int or a str.
                 2) If year is not given as an int.
@@ -62,7 +69,7 @@ class Month:
                           "Dec": 12}
             month_names = [i for i in month_dict.keys()]
             try:
-                self.month = month_dict[month]
+                self._month = month_dict[month]
             except KeyError:
                 raise ValueError(
                     f"If given as a str, month has to be one of the "
@@ -70,38 +77,77 @@ class Month:
                     f"{', '.join(month_names[-12:])}, not '{month}'")
         elif isinstance(month, int):
             if 1 <= month <= 12:
-                self.month = month
+                self._month = month
             else:
                 raise ValueError(
                     f"If given as an int, year has to be in [0, 11] interval"
                 )
         else:
             raise TypeError(
-                f"Month has to be given as a str or an int, "
+                f"month has to be given as a str or an int, "
                 f"not {type(month)}"
             )
         if isinstance(year, int):
             if 0 <= year <= 99:
-                self.year = 2000+year
+                self._year = 2000+year
             elif year >= 1000:
-                self.year = year
+                self._year = year
             else:
                 raise ValueError(
-                    f"Year has to be either in [0, 99] range or greater "
+                    f"year has to be either in [0, 99] range or greater "
                     f"than or equal to 1000, not {year}"
                 )
         else:
             raise TypeError(
-                f"Year has to be given as an int, "
+                f"year has to be given as an int, "
                 f"not {type(year)}"
             )
-        self.starting_day = datetime.strptime(f"{self.year}-{self.month}",
-                                              "%Y-%m").weekday()
-        print(self.starting_day)
+        self._starting_day, self._day_count = calendar.monthrange(self._year,
+                                                                  self._month)
+        if len(colors) == self._day_count:
+            self._colors = colors
+            self._colors = [None]*self._starting_day + self._colors
+            self._colors += [None]*(42-len(self._colors))
+        else:
+            raise ValueError(
+                f"The amount of colors ({len(colors)}) "
+                f"has to be equal to the amount of days ({self._day_count})"
+            )
+        if isinstance(neutral_color, str):
+            self._neutral_color = neutral_color
+        else:
+            raise TypeError(
+                f"neutral_color has to be given as a str, "
+                f"not {type(neutral_color)}"
+            )
+        self._image = None
+        self.paint()
+
+    def paint(self):
+        self._image = Image.new("RGB", (780, 670), (255, 255, 255))
+        draw = ImageDraw.Draw(self._image)
+        coordinates = [10, 10]
+        count = 0
+        for color in self._colors:
+            if color is not None:
+                draw.rectangle((coordinates[0], coordinates[1],
+                                coordinates[0]+100, coordinates[1]+100),
+                               fill=color)
+            else:
+                draw.rectangle((coordinates[0], coordinates[1],
+                                coordinates[0] + 100, coordinates[1] + 100),
+                               fill=self._neutral_color)
+            coordinates[0] += 110
+            count += 1
+            if count == 7:
+                coordinates[0] = 10
+                coordinates[1] += 110
+                count = 0
+        self._image.save("test.png")
 
 
 def main():
-    month = Month(9, 2023)
+    month = Month(9, 2023, ["#FCB1A6"]*30)
 
 
 if __name__ == "__main__":
